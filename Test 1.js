06ed9d3cc86ab86c1a15e3caed71a2919a12dd88
IsaacSoares <54124613+IsaacSoares@users.noreply.github.com>
2023-07-28 17:17:00 -0300

    Test 1

---
+++ Test 1.js
@@ -0,0 +1,11 @@
+const Trocap = document.querySelector("#ptochange")
+const button = document.querySelector("#bchange")
+
+button.addEventListener("click", function(){ 
+
+Trocap.innerHTML = "Sorry, i'm Isaac Soares."
+
+}
+
+)
+
